add8285906ce62e6d5aeecd4474d0b70cc403464
Bizzle Dapp <36421291+Bizzle-Dapp@users.noreply.github.com>
2021-01-27 22:27:43 +0000

    Tested Routing to Time on App from Home.

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,8 +2,39 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
+
+beforeAll(() => {
+  jest.useFakeTimers();
+});
+
+afterEach(() => {
+  jest.clearAllTimers();
+});
+
+afterAll(() => {
+  jest.useRealTimers();
+});
+
 // Big brain, GO!
 test('Determines the truthyness of being true', () => {
   render(<App />);
   expect(true).toBeTruthy();
 });
+
+// Now for the real big brain, going to try and test a timer... 
+test('If Routing from App to Time on App works.',  () => {
+  console.log("Mounting App...")
+  render(<App />)
+  let header = screen.getByText((content, element) => {
+    return element?.tagName.toLowerCase() === 'h1' && content.startsWith('Home');
+  })
+  console.log("Looking for H1 element with 'Home'")
+  expect(header).toBeDefined();
+  console.log("Clicking 'Time on App' Button.")
+  screen.getByText('Time on App').click();
+  header = screen.getByText((content, element) => {
+    return element?.tagName.toLowerCase() === 'h1' && content.startsWith('Time on App');
+  })
+  console.log("Looking for H1 element with 'Time on App'")
+  expect(header).toBeDefined();
+})
