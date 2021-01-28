ebaf142827f680d3a03b65d66324d730ba7fe627
Bizzle Dapp <36421291+Bizzle-Dapp@users.noreply.github.com>
2021-01-28 16:44:33 +0000

    Removed wasted timer references in tests

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -3,18 +3,6 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 
-beforeAll(() => {
-  jest.useFakeTimers();
-});
-
-afterEach(() => {
-  jest.clearAllTimers();
-});
-
-afterAll(() => {
-  jest.useRealTimers();
-});
-
 // Big brain, GO!
 test('Determines the truthyness of being true', () => {
   render(<App />);
