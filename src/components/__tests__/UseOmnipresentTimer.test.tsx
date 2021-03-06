f6509f333e655193a0c19555e599f0d8dee19e38
Luke Schofield <lschofield1991@gmail.com>
2021-03-06 11:15:33 +0000

    waitForValueToChange timeout used instead of useFakeTimers.

--- src/components/__tests__/UseOmnipresentTimer.test.tsx
+++ src/components/__tests__/UseOmnipresentTimer.test.tsx
@@ -1,23 +1,15 @@
 import { act, renderHook } from '@testing-library/react-hooks';
 import useOmnipresentTimer from '../useOmnipresentTimer';
 
-afterEach(() => {
-    jest.clearAllMocks()
-  })
-
 describe("App holds a continuous timer tick", () => {
     test("if after a second has passed, the interval has increased", async () => {
         const { result, waitForValueToChange } = renderHook(() => useOmnipresentTimer())
-        
-        act(() => {
-            jest.useFakeTimers();
-        });
 
         expect(result.current.timeOnApp).toBe(0);
         
         await waitForValueToChange(() => {
             return result.current.timeOnApp;
-        });
+        }, {timeout: 2000});
         
         expect(result.current.timeOnApp).toBe(1);
     })
