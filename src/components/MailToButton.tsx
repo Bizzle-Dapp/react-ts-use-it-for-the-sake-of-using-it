01975cf7cc71134e44ab45f6dbbada31390d040e
Bizzle_Dapp <lschofield1991@gmail.com>
2022-05-13 14:42:06 +0100

    Prototype mailto handler and failure handler

--- src/components/MailToButton.tsx
+++ src/components/MailToButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 enum MailToValues {
     subject = `This is an Email Subject`,
@@ -7,34 +7,101 @@ enum MailToValues {
     \nIf i also mess around further with double linebreaks and the like, we might be onto something.
     \n
     \nWhat about adding enbolded unicode characters: 𝐝𝐝𝐝 
-    \nOr even Emoji: 🤖`
-
+    \nOr even Emoji: 🤖
+    `
 }
-
 
 const MailTo: React.FC = () => {
 
-    const openMailClient = (to: string, 
-                            cc: string, 
-                            bcc: string, 
-                            subject: string, 
-                            body: string ) => {
-        window.location.href = 
-        `mailto:${to}?cc=${cc}&bcc=${bcc}&subject=${subject}&body=${encodeURIComponent(body)}`;
+    const [opening, setOpening] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [mailClientTriggerWait, setMailClientTriggerWait] = useState(0);
+    const [controller] = useState(new AbortController());
+    
+    const [error, setError] = useState('');
+    const [length, setLength] = useState(0);
+    const openMailClient = (to: string,
+        cc: string,
+        bcc: string,
+        subject: string,
+        body: string) => {
+        const uriValue = `mailto:${to}?cc=${cc}&bcc=${bcc}&subject=${subject}&body=${encodeURIComponent(body)}`;
+        setLength(uriValue.length);
+        window.location.href =
+            `mailto:${to}?cc=${cc}&bcc=${bcc}&subject=${subject}&body=${encodeURIComponent(body)}`;
     }
 
-    return(
-        <button
-            onClick={() => {openMailClient(
-                'example@example.com',
-                'cc@example.com',
-                'bcc@example.com',
-                MailToValues.subject,
-                MailToValues.body
-            )}}
-        >
-            Send Mail
-        </button>
+    const onTriggerEffect = (event: any) => {
+        console.log("Triggered", event);
+        setError(``)
+        setSuccess(true);
+        setOpening(false);
+        setMailClientTriggerWait(0)
+        controller.abort();
+    }
+
+    // Using the window focus to detect if the mail client opened in a period of time
+    const handleMailClientOpenTracking = (to: string,
+        cc: string,
+        bcc: string,
+        subject: string,
+        body: string) => {
+        setOpening(true);
+        setSuccess(false);
+
+        window.addEventListener('blur', onTriggerEffect, { signal: controller.signal } as AddEventListenerOptions);
+        openMailClient(to, cc, bcc, subject, body);
+    }
+
+
+    useEffect(() => {
+        if (opening) {
+            const failTriggerEffect = () => {
+                console.log("Triggered Closure", mailClientTriggerWait)
+                setError(
+                    `The mail client is either taking abnormally long to open or has failed to open.
+                If it has indeed failed, this could be due to a long data object being sent. 
+                Please reduce the length of the URI to below 2000.
+                `)
+                setOpening(false);
+                controller.abort();
+            };
+            let intervalId = window.setInterval(() => {
+                setMailClientTriggerWait(value => value + 1);
+                console.log("Tick", mailClientTriggerWait);
+            }, 1000);
+            if(success){
+                window.clearInterval(intervalId)
+                return;
+            }
+            if (mailClientTriggerWait === 4) {
+                failTriggerEffect();
+                window.clearInterval(intervalId)
+                return;
+            }
+            return () => window.clearInterval(intervalId)
+        }
+    }, [opening, success, mailClientTriggerWait])
+
+    return (
+        <div>
+            <button
+                onClick={() => {
+                    handleMailClientOpenTracking(
+                        'example@example.com',
+                        'cc@example.com',
+                        'bcc@example.com',
+                        MailToValues.subject,
+                        MailToValues.body
+                    )
+                }}
+                disabled={opening}
+            >
+                Send Mail
+            </button>
+            <p>Last URI Length: {length}</p>
+            <p style={{ color: 'darkred' }}>{error}</p>
+        </div>
     )
 }
 
