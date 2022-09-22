968ffa333d14179b3483faf03bc47c88bcd8c69b
Olys6 <oliver@skjevesland.com>
2022-09-22 14:48:01 +0100

    added cypress tests that change the image on the home page and interact with all inputs on the form page

--- cypress/e2e/3-learn/home.cy.js
+++ cypress/e2e/3-learn/home.cy.js
@@ -1,6 +1,13 @@
 /// <reference types="cypress" />
 
 describe('my awesome website', () => {
+
+  before(function () {
+    cy.fixture('example').then(function (data) {
+      this.data = data;
+    })
+  })
+
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
@@ -8,11 +15,32 @@ describe('my awesome website', () => {
   it('turns page to dark mode', () => {
     cy.get('.Theme-Toggle').check()
   })
-  it('turns page to light mode', () => {
-    cy.get('.Theme-Toggle').uncheck()
+
+  // it('turns page to light mode', () => {
+  //   cy.get('.Theme-Toggle').uncheck()
+  // })
+
+  it('checks that page header is correct', () => {
+    cy.get('code').contains("React TS Use It For The Sake Of Using It")
   })
 
-  it('sorts numbers', () => {
-    cy.get(':nth-child(9) > .Details-Form-Input').type('10523')
+  it('changes the image to gif of a dog', () => {
+    cy.get('[data-testid="Image-Path-Input"]').clear().type("https://i.imgur.com/05UQ84w.gif")
   })
+
+  it('goes to details form page and configures all inputs', () => {
+    cy.get('[href="/detailsForm"] > button').click()
+
+    cy.get('#selectOption').select('Option 2')
+
+    cy.get('[data-testid="Age-Form-Input"]').type("{backspace}").type("20")
+    cy.get('[data-testid="Name-Form-Input"]').type("Olys6")
+    cy.get('[data-testid="Email-Form-Input"]').type("oliver.skjevesland@gmail.com")
+
+    cy.get('[data-testid="Form-Clear-Button"]').click()
+  })
+
+  // it('changes select to input 2 from input 1', () => {
+    
+  // })
 })
